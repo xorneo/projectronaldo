53f348d057bf7aeaf6f77ced21dd327b785d84e2
xorneo <xorneo1@gmail.com>
2024-05-31 19:04:08 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-// supabase-config.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ecwwruovdowlqdtvktaz.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY; // Ensure this is set up in your environment variables
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
